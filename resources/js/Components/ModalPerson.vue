070e1fa7ab798bd9d3f39ed618df17f669f52968
pedroguaillas <peter.tufi@gmail.com>
2023-05-22 18:45:16 -0500

    Save Persona

--- resources/js/Components/ModalPerson.vue
+++ resources/js/Components/ModalPerson.vue
@@ -1,8 +1,10 @@
 <script setup>
-import { reactive } from 'vue';
-import TextInput from './TextInput.vue';
+import { reactive } from 'vue'
+import DialogModal from './DialogModal.vue'
+import TextInput from './TextInput.vue'
+import InputError from './InputError.vue'
 
-const emit = defineEmits(['close']);
+const emit = defineEmits(['close', 'confirmed'])
 
 defineProps({
     person: { type: Object, default: () => ({}) },
@@ -10,17 +12,10 @@ defineProps({
         type: Boolean,
         default: false
     },
-    op: {
-        type: String,
-        default: ''
-    },
-    title: {
-        type: String,
-        default: ''
-    }
+    op: Number
 })
 
-const form = reactive({
+const initialState = {
     identification: '',
     first_name: '',
     last_name: '',
@@ -30,116 +25,161 @@ const form = reactive({
     latitude: '',
     longitude: '',
     error: { type: Object, default: () => ({}) },
-    processing: false,
-});
+    processing: false
+}
+
+const form = reactive({ ...initialState })
+
+const resetForm = () => {
+    Object.assign(form, initialState)
+}
 
 const save = () => {
-    form.processing = true;
+    form.processing = true
 
-    axios.post(route('people.store'), {
-        identification: form.identification,
-        first_name: form.first_name,
-        last_name: form.last_name,
-        birthdate: form.birthdate,
-        phone: form.phone,
-        civil_status: form.civil_status,
-        latitude: form.latitude,
-        longitude: form.longitude,
-    }).then(() => {
-        form.processing = false;
+    axios
+        .post(route('people.store'), {
+            identification: form.identification,
+            first_name: form.first_name,
+            last_name: form.last_name,
+            birthdate: form.birthdate,
+            phone: form.phone,
+            civil_status: form.civil_status,
+            latitude: form.latitude,
+            longitude: form.longitude
+        })
+        .then(() => {
+            form.processing = false
 
-        close();
-        // form.reset();
-        nextTick().then(() => emit('confirmed'));
+            close()
+            resetForm()
+            emit('confirmed')
+        })
+        .catch(error => {
+            form.processing = false
 
-    }).catch(error => {
-        form.processing = false;
-        form.error = error.response.data.errors;
-        // passwordInput.value.focus();
-    });
+            Object.keys(error.response.data.errors).forEach(key => {
+                form.error[key] = error.response.data.errors[key][0]
+            })
+        })
 }
 
 const update = () => {
-    var id = document.getElementById('id2').value;
+    var id = document.getElementById('id2').value
     // form.put(route('people.update', id));
 }
 
 const close = () => {
-    emit('close');
-};
+    emit('close')
+}
 </script>
 
 <template>
-    <div class="container mx-auto">
-        <div class="flex justify-center">
-            <div v-show="isOpen" class="
-            absolute
-            inset-0
-            flex
-            items-center
-            justify-center
-            bg-gray-700 bg-opacity-50
-          ">
-                <div class="max-w-2xl p-6 bg-white rounded-md shadow-xl">
-                    <div class="flex items-center justify-between">
-                        <h3 class="text-2xl">{{ title }}</h3>
-                        <svg @click="close" xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-red-900 cursor-pointer"
-                            fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
-                    </div>
-                    <div class="mt-4">
-                        <form @submit.prevent="(op === '1' ? save() : update())" class="w-2xl">
+    <DialogModal :show="isOpen" maxWidth="lg" @close="close">
+        <template #title>
+            {{ `${op > 0 ? 'Editar' : 'Añadir'} persona` }}
+        </template>
+        <template #content>
+            <div class="mt-4">
+                <form
+                    @submit.prevent="op > 1 ? update() : save()"
+                    class="w-2xl"
+                >
+                    <TextInput
+                        :id="'id' + op"
+                        type="hidden"
+                        name="id"
+                        v-model="form.id"
+                    ></TextInput>
 
-                            <TextInput :id="'id' + op" type="hidden" name="id" v-model="form.id"></TextInput>
+                    <TextInput
+                        v-model="form.identification"
+                        type="text"
+                        class="mt-1 block w-full"
+                        placeholder="Cédula"
+                        minlength="10"
+                        maxlength="10"
+                    />
+                    <InputError
+                        :message="form.error.identification"
+                        class="mt-2"
+                    />
 
-                            <input v-model="form.identification" type="text" class="mt-1 block w-2xl" placeholder="Cédula"
-                                minlength="10" maxlength="10" />
-                            <InputError :message="form.error.identification" class="mt-2" />
+                    <TextInput
+                        v-model="form.first_name"
+                        type="text"
+                        class="mt-1 block w-full"
+                        placeholder="Nombre"
+                    />
+                    <InputError :message="form.error.first_name" class="mt-2" />
 
-                            <TextInput v-model="form.first_name" type="text" class="mt-1 block w-full"
-                                placeholder="Nombre" />
-                            <InputError :message="form.error.first_name" class="mt-2" />
+                    <TextInput
+                        v-model="form.last_name"
+                        type="text"
+                        class="mt-1 block w-full"
+                        placeholder="Apellido"
+                    />
+                    <InputError :message="form.error.last_name" class="mt-2" />
 
-                            <TextInput v-model="form.last_name" type="text" class="mt-1 block w-full"
-                                placeholder="Apellido" />
-                            <InputError :message="form.error.last_name" class="mt-2" />
+                    <TextInput
+                        v-model="form.birthdate"
+                        type="date"
+                        class="mt-1 block w-full"
+                        placeholder="Fecha de nacimiento"
+                    />
+                    <InputError :message="form.error.birthdate" class="mt-2" />
 
-                            <TextInput v-model="form.birthdate" type="date" class="mt-1 block w-full"
-                                placeholder="Fecha de nacimiento" />
-                            <InputError :message="form.error.birthdate" class="mt-2" />
+                    <TextInput
+                        v-model="form.phone"
+                        type="text"
+                        class="mt-1 block w-full"
+                        placeholder="Celular"
+                        minlength="10"
+                        maxlength="10"
+                    />
+                    <InputError :message="form.error.phone" class="mt-2" />
 
-                            <input v-model="form.phone" type="text" class="mt-1 block w-full" placeholder="Celular"
-                                minlength="10" maxlength="10" />
-                            <InputError :message="form.error.phone" class="mt-2" />
+                    <select
+                        v-model="form.civil_status"
+                        class="mt-1 block w-full rounded border-gray-300"
+                    >
+                        <option value="">Seleccione</option>
+                        <option value="Soltero">Soltero</option>
+                        <option value="Casado">Casado</option>
+                        <option value="Divorciado">Divorciado</option>
+                        <option value="Viudo">Viudo</option>
+                    </select>
+                    <InputError
+                        :message="form.error.civil_status"
+                        class="mt-2"
+                    />
 
-                            <select v-model="form.civil_status" class="mt-1 block w-full">
-                                <option value="">Seleccione</option>
-                                <option value="Soltero">Soltero</option>
-                                <option value="Casado">Casado</option>
-                                <option value="Divorciado">Divorciado</option>
-                                <option value="Viudo">Viudo</option>
-                            </select>
-                            <InputError :message="form.error.civil_status" class="mt-2" />
+                    <TextInput
+                        v-model="form.latitude"
+                        type="text"
+                        class="mt-1 block w-full"
+                        placeholder="Latitud"
+                        minlength="1"
+                        maxlength="15"
+                    />
+                    <InputError :message="form.error.latitude" class="mt-2" />
 
-                            <input v-model="form.latitude" type="text" class="mt-1 block w-full" placeholder="Latitud"
-                                minlength="1" maxlength="15" />
-                            <InputError :message="form.error.latitude" class="mt-2" />
-
-                            <input v-model="form.longitude" type="text" class="mt-1 block w-full" placeholder="Longitud"
-                                minlength="1" maxlength="15" />
-                            <InputError :message="form.error.longitude" class="mt-2" />
-
-                            <div class="flex justify-end mt-4">
-                                <button class="px-6 py-2 ml-2 text-blue-100 bg-blue-600 rounded">
-                                    Guardar
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+                    <TextInput
+                        v-model="form.longitude"
+                        type="text"
+                        class="mt-1 block w-full"
+                        placeholder="Longitud"
+                        minlength="1"
+                        maxlength="15"
+                    />
+                    <InputError :message="form.error.longitude" class="mt-2" />
+                </form>
             </div>
-        </div>
-    </div>
+        </template>
+        <template #footer>
+            <button class="px-6 py-2 ml-2 text-blue-100 bg-blue-600 rounded">
+                Guardar
+            </button>
+        </template>
+    </DialogModal>
 </template>
--- resources/js/Pages/Person/Index.vue
+++ resources/js/Pages/Person/Index.vue
@@ -1,27 +1,34 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import { useForm } from '@inertiajs/vue3';
-import ModalPerson from '../../Components/ModalPerson.vue';
-import { ref } from 'vue';
-import Swal from 'sweetalert2';
+import AppLayout from '@/Layouts/AppLayout.vue'
+import { router, useForm } from '@inertiajs/vue3'
+import ModalPerson from '../../Components/ModalPerson.vue'
+import { ref } from 'vue'
+import Swal from 'sweetalert2'
 
 defineProps({
     people: { type: Object }
-});
+})
 
-const modal = ref(false);
+const modal = ref(false)
+let op = ref(-1)
 
-const form = useForm({});
+const form = useForm({})
 
-const openModal = () => {
-    modal.value = true;
-};
+const openModal = opValue => {
+    op = opValue
+    modal.value = true
+}
 
 const closeModal = () => {
-    modal.value = false;
-};
+    op = -1
+    modal.value = false
+}
 
-const editar = () => { }
+const confirmed = () => {
+    router.reload()
+}
+
+const editar = () => {}
 
 const eliminar = (id, name) => {
     const swal = Swal.mixin({
@@ -34,21 +41,12 @@ const eliminar = (id, name) => {
         icon: 'question',
         showCancelButton: true,
         confirmButtonText: '<i class="fa-solid fa-check"> </i> Si, eliminar',
-        cancelButtonText: '<i class="fa-solid fa-ban"> </i> Cancelar',
-    }).then((result) => {
+        cancelButtonText: '<i class="fa-solid fa-ban"> </i> Cancelar'
+    }).then(result => {
         if (result.isConfirmed) {
             form.delete(route('people.destroy', id))
         }
     })
-
-    // axios.delete(route('people.destroyed/' + id)).then(() => {
-    //     close();
-    //     nextTick().then(() => emit('confirmed'));
-
-    // }).catch(error => {
-    //     form.error = error.response.data.errors;
-    //     // passwordInput.value.focus();
-    // });
 }
 </script>
 
@@ -57,7 +55,10 @@ const eliminar = (id, name) => {
         <template #header>
             <div class="container mx-auto bg-white w-full flex">
                 <h1><i class="fa fa-users"></i> Personas</h1>
-                <button @click="openModal" class="ml-2 h-8 px-3 rounded bg-green-500 text-white">
+                <button
+                    @click="openModal(0)"
+                    class="ml-2 h-8 px-3 rounded bg-green-500 text-white"
+                >
                     <i class="fa-solid fa-circle-plus"></i> Añadir
                 </button>
             </div>
@@ -67,33 +68,49 @@ const eliminar = (id, name) => {
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                     <div class="p-4">
-
-                        <table class="w-full table-auto">
+                        <table class="w-full table-auto border-collapse border border-gray-300">
                             <thead>
                                 <tr>
-                                    <th>Apellido</th>
-                                    <th>Nombre</th>
-                                    <th>Fecha de nacimiento</th>
-                                    <th>Celular</th>
-                                    <th>Estado Civil</th>
-                                    <th>JUbicación</th>
-                                    <th>Acción</th>
+                                    <th class="border border-gray-300">Apellido</th>
+                                    <th class="border border-gray-300">Nombre</th>
+                                    <th class="border border-gray-300">Fecha nacimiento</th>
+                                    <th class="border border-gray-300">Celular</th>
+                                    <th class="border border-gray-300">Estado Civil</th>
+                                    <th class="border border-gray-300">JUbicación</th>
+                                    <th class="border border-gray-300">Acción</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr v-for="person, i in people" :key="person.id">
-                                    <td>{{ person.last_name }}</td>
-                                    <td>{{ person.first_name }}</td>
-                                    <td>{{ person.birthdate }}</td>
-                                    <td>{{ person.phone }}</td>
-                                    <td>{{ person.civil_status }}</td>
-                                    <td>{{ `${person.latitude}, ${person.longitude}` }}</td>
-                                    <td>
-                                        <button @click="editar" class="bg-yellow-500 p-1 rounded">
+                                <tr
+                                    v-for="(person, i) in people"
+                                    :key="person.id"
+                                >
+                                    <td class="border border-gray-300 p-1">{{ person.last_name }}</td>
+                                    <td class="border border-gray-300 p-1">{{ person.first_name }}</td>
+                                    <td class="border border-gray-300 p-1 text-center">{{ person.birthdate }}</td>
+                                    <td class="border border-gray-300 p-1 text-center">{{ person.phone }}</td>
+                                    <td class="border border-gray-300 p-1 text-center">{{ person.civil_status }}</td>
+                                    <td class="border border-gray-300 p-1 text-center">
+                                        {{
+                                            `${person.latitude}, ${person.longitude}`
+                                        }}
+                                    </td>
+                                    <td class="border border-gray-300 p-1 text-center">
+                                        <button
+                                            @click="openModal(person.id)"
+                                            class="bg-yellow-500 py-1 px-2 text-white rounded"
+                                        >
                                             <i class="fa-solid fa-edit"></i>
                                         </button>
-                                        <button @click="eliminar(person.id, person.first_name)"
-                                            class="ml-1 bg-red-500 p-1 rounded">
+                                        <button
+                                            @click="
+                                                eliminar(
+                                                    person.id,
+                                                    person.first_name
+                                                )
+                                            "
+                                            class="ml-1 bg-red-500 py-1 px-2 text-white rounded"
+                                        >
                                             <i class="fa-solid fa-trash"></i>
                                         </button>
                                     </td>
@@ -105,6 +122,10 @@ const eliminar = (id, name) => {
             </div>
         </div>
     </AppLayout>
-    <ModalPerson :isOpen="modal" :op="'1'" :title="'Añadir Persona'" @close="closeModal"></ModalPerson>
-    <ModalPerson :isOpen="modal" :op="'2'" :title="'Editar Persona'" @close="closeModal"></ModalPerson>
+    <ModalPerson
+        :isOpen="modal"
+        :op="op"
+        @close="closeModal"
+        @confirmed="confirmed"
+    ></ModalPerson>
 </template>
